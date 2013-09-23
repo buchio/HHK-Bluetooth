823636d7a8aaf8f76f366ec03d7a877cb8471df8
Yukio Obuchi <buchio@buchio.com>
2013-09-24 06:45:25 +0900

    共通ライブラリプロジェクト追加

---
+++ Software/Common.X/common_uart.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   uart.h
+ * Author: buchio
+ *
+ * Created on 2013/09/24, 5:59
+ */
+
+#ifndef UART_H
+#define	UART_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    void uart_initialize();
+    
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* UART_H */
+
